6805639a21bbdd172159bbbe5dbf1c988afcac45
IDHSRG <105148723+IDHSRG@users.noreply.github.com>
2024-02-08 11:17:50 +0300

    Создал Emoji.cs
    Класс для определения эмодзи для интерфейса

---
+++ program/Emoji.cs
@@ -0,0 +1,12 @@
+public class Emoji
+{
+  public Emoji(string path)
+  {
+    //Создание эмодзи по пути к изображению
+  }
+
+  public static BitMap Compress(string path)
+  {
+    //Сжатие до размера эмодзи указанного в пути изображения
+  }
+}
